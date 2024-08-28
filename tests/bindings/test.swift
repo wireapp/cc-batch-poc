76f398417f53a9c313e232adffee15726061a7c6
Jacob Persson <7156+typfel@users.noreply.github.com>
2024-08-28 22:50:45 +0200

    Add Swift example for how to provide a nicer transaction function

--- tests/bindings/test.swift
+++ tests/bindings/test.swift
@@ -5,20 +5,41 @@ var task = DispatchGroup()
 var cc = CoreCrypto()
 task.enter()
 Task {
-    class TestTransaction: CoreCryptoCommand {
-        func execute(context: CoreCryptoContext) async throws {
-            print(try await context.decrypt(convId: "conv1", msg: Data("hello world".utf8)))
-            var failed = false
-            do {
-                print(try await context.decrypt(convId: "id", msg: Data("hello world".utf8)))
-            } catch {
-                failed = true
-                print("Expected error: \(error)")
-            }
-            assert(failed)
+    if let result = try! await cc.niceTransaction({ context in
+        print(try await context.decrypt(convId: "conv1", msg: Data("hello world".utf8)))
+        var failed = false
+        do {
+            print(try await context.decrypt(convId: "id", msg: Data("hello world".utf8)))
+        } catch {
+            failed = true
+            print("Expected error: \(error)")
         }
+        assert(failed)
+        return 5
+    }) {
+        print("result: \(result)")
     }
-    try! await cc.transaction(command: TestTransaction())
     task.leave()
 }
 task.wait()
+
+class TransactionExecutor<Result>: CoreCryptoCommand {
+    let block: (_ context: CoreCryptoContext) async throws -> Result
+    var result: Result?
+
+    init(_ block: @escaping (_ context: CoreCryptoContext) async throws -> Result) {
+        self.block = block
+    }
+
+    func execute(context: CoreCryptoContext) async throws {
+        result = try await block(context)
+    }
+}
+
+extension CoreCrypto {
+    func niceTransaction<Result>(_ block: @escaping (_ context: CoreCryptoContext) async throws -> Result) async throws -> Result? {
+        let transactionExecutor = TransactionExecutor<Result>(block)
+        try await transaction(command: transactionExecutor)
+        return transactionExecutor.result
+    }
+}
